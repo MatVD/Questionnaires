e7ce5a316366b61ec1daafead4076c4d1439448c
MatVD <vandammemathieu@hotmail.fr>
2022-03-17 13:51:20 +0100

    ajout questionnaire version POO

---
+++ questionnaireV3.py
@@ -0,0 +1,41 @@
+# Questionnaire version 3 - orientée objet
+
+
+class Question:
+
+    NB_QUESTIONS = 0
+    SCORE = 0
+
+    def __init__(self, titre_question, choix_de_reponse, bonne_reponse):
+        Question.NB_QUESTIONS += 1
+        self.titre_question = titre_question
+        self.choix_de_reponse = choix_de_reponse
+        self.bonne_reponse = bonne_reponse
+
+    def affichage(self):
+        print()
+        print(f"Question {Question.SCORE + 1}: {self.titre_question}")
+        print(f"{', '.join(self.choix_de_reponse)}")
+        self.reponse = input("Votre réponse: ")
+        self.gestion_score()
+
+    def gestion_score(self):
+        print()
+        if self.reponse.lower() == self.bonne_reponse.lower():
+            Question.SCORE +=1
+            print("Bonne réponse !")
+        else: 
+            print("Mauvaise réponse !")
+        print(f"Votre score est de {Question.SCORE}/{Question.NB_QUESTIONS}")
+        print()
+
+
+liste_questions = [
+    Question("Quelle est la capitale de la France? ", ("Nantes", "Lyon", "Paris", "Marseille"), "Paris"),
+    Question("Quelle est la capitale de l'Italie? ", ("Turin", "Naples", "Rome", "Venise"), "Rome"),
+    Question("Quelle est la capitale de l'Espagne? ", ("Barcelone", "Madrid", "Salou", "Seville"), "Madrid"),
+]
+
+for question in liste_questions:
+    question.affichage()
+
